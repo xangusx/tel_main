189a3955e9b425b054d9c653ed60a0a965bb71c0
Leokang0801 <bartkang0818@gmail.com>
2022-10-19 17:08:07 +0000

    version_10.20.2

--- race/src/second.cpp
+++ race/src/second.cpp
@@ -8,10 +8,10 @@ void second()
     MoveTo(80,18,0.5,0.01);
     // distance = vl53(2);
     // odom_init();
-    Move(0,0.1,0);//move to wall
-    micro_move(-1,25,-1,0.1,0.02);
-    MoveTo(140,25,0.5,0.01);
-    MoveTo(140,50,0.5,0.01);
+    Move(0,0.15,0);//move to wall
+    micro_move(100,25,-1,0.2,0.1);
+    MoveTo(150,25,0.5,0.01);
+    MoveTo(150,50,0.5,0.01);
     MoveTo(250,50,0.5,0.01);
     MoveTo(250,20,0.5,0.01);
     MoveTo(350,20,0.5,0.01);
