f03e2e72c149cd6f893fe6c80edd6a3b539b6091
Leokang0801 <bartkang0818@gmail.com>
2022-10-20 19:48:28 +0000

    version_10.21.2

--- race/include/imu_climb.h
+++ race/include/imu_climb.h
@@ -9,17 +9,19 @@ void imu_rotate(float imu_yaw_init);
 
 ros::Subscriber imu_sub;
 ros::Publisher imu_vel_pub;
+ros::Subscriber imu_sub_1;
 // sensor_msgs::Imu imu;
 geometry_msgs::Point angle;
+geometry_msgs::Point angle_1;
 geometry_msgs::Twist imu_vel;
 
 float accel_p_c = 0.005;
 float accel_n_c = 0.01;
 float accel_spin;
-float Maxvel_climb = 0.5;
+float Maxvel_climb = 1.6;
 float Maxvel_spin;
-float Minvel_spin = 0.01;
+float Minvel_spin;
 float imu_yaw_init;
 float spin_allow = 0.1;
 float angle_diff;
-float plane_angle = -1;
+float plane_angle = 0;
--- race/src/imu_climb.cpp
+++ race/src/imu_climb.cpp
@@ -5,6 +5,12 @@ void imu_callback(const geometry_msgs::Point::ConstPtr &msg){
     angle.y = msg->x;
     angle.z = msg->z;
 }
+void imu_1_callback(const geometry_msgs::Point::ConstPtr &msg1){
+    angle_1.x = msg1->y;
+    angle_1.y = msg1->x;
+    angle_1.z = msg1->z;
+}
+
 
 
 int main(int argc, char** argv){
@@ -14,6 +20,7 @@ int main(int argc, char** argv){
     
     imu_vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel",1);
     imu_sub = nh.subscribe("imu_angle", 1, imu_callback);
+    imu_sub_1 = nh.subscribe("imu_angular", 1, imu_1_callback);
     ros::Rate rate(100);
 
     while(ros::ok()){
@@ -32,15 +39,12 @@ int main(int argc, char** argv){
             imu_vel_pub.publish(imu_vel);
             rate.sleep();
         }
-        std::cout << angle.x <<"\n";
 
-        while(angle.x < plane_angle && ros::ok()){   //衝上上坡
+        while(angle.x <= plane_angle && ros::ok()){   //衝上上坡
             ros::spinOnce(); 
             imu_vel.linear.x = Maxvel_climb;
             imu_vel_pub.publish(imu_vel);
-            // std::cout << "66666\n";
         }
-        std::cout << angle.x <<"\n";
 
         while(angle.x >= plane_angle && ros::ok()){   //煞車
             ros::spinOnce();
@@ -64,27 +68,33 @@ int main(int argc, char** argv){
 void imu_rotate(float imu_yaw_init){
 
     ros::spinOnce();
-    angle_diff = angle.z - imu_yaw_init;
+    angle_diff = angle_1.z - imu_yaw_init;
     Maxvel_spin = angle_diff*0.001;
     accel_spin = Maxvel_spin*0.1;
+    Minvel_spin = 0.01;
+    if(angle_diff < 0){
+        angle_diff = -angle_diff;
+        Minvel_spin = -0.01;
+    }
     
-    while(ros::ok() && angle.z-imu_yaw_init>0.5*angle_diff){
+    while(ros::ok() && fabs(angle_1.z-imu_yaw_init)>0.5*angle_diff){
         ros::spinOnce();
-        if(imu_vel.angular.z < Maxvel_spin){
+        if(fabs(imu_vel.angular.z) < fabs(Maxvel_spin)){
             imu_vel.angular.z += accel_spin;
-        }else if(imu_vel.angular.z >= Maxvel_spin){
+        }else if(fabs(imu_vel.angular.z) >= fabs(Maxvel_spin)){
             imu_vel.angular.z = Maxvel_spin;
         }
         imu_vel_pub.publish(imu_vel);
     }
-    while(ros::ok() && angle.z-imu_yaw_init<=0.5*angle_diff){
+
+    while(ros::ok() && fabs(angle_1.z-imu_yaw_init)<=0.5*angle_diff){
         ros::spinOnce();
-        if(angle.z-imu_yaw_init <= spin_allow){
+        if(fabs(angle.z-imu_yaw_init) <= spin_allow){
             imu_vel.angular.z = 0;
             imu_vel_pub.publish(imu_vel);
             break;
         }else{
-            if(imu_vel.angular.z > Minvel_spin){
+            if(fabs(imu_vel.angular.z) > fabs(Minvel_spin)){
                 imu_vel.angular.z -= accel_spin;
             }else{
                 imu_vel.angular.z = Minvel_spin;
