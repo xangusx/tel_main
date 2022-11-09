31a090257f8cb4a83fa71664b41d85ddd4329bd4
Leokang0801 <bartkang0818@gmail.com>
2022-11-09 15:43:33 +0000

    version_11.09.2

--- race/src/first.cpp
+++ race/src/first.cpp
@@ -2,7 +2,6 @@
 
 void first()
 {
-
     // first_state = Scara_move(0,0,0,first_state);    
     // // Scara_move(0,29.0832266,0);
     // first_state = Scara_move(-8,22,1,first_state);
@@ -13,22 +12,26 @@ void first()
     
     // MoveTo(x,y,max_vel,acceleration);
     // first_move(60);
-    rotation(-90,0.01,0.001); 
-    rotation(0,0.01,0.001);
-    MoveTo(50,0,0,0.5,0.01);
-    MoveTo(0,50,90,0.5,0.01);
-    MoveTo(0,0,0,0.5,0.01);
-    // rotation(90,0.01,0.001);
 
-    //test
-    // MoveTo(-20,0,0.5,0.02);
-    // MoveTo(-20,0,0.5,0.02);
-    // MoveTo(0,0,0.5,0.02);
-    // MoveTo(0,20,0.5,0.02);
-    // MoveTo(0,-20,0.5,0.02);
-    // MoveTo(20,0,0.5,0.02);
-    // MoveTo(0,20,0.5,0.02);
-    // MoveTo(-20,0,0.5,0.02);
-    // MoveTo(0,0,0.5,0.02);
-    // vl53(left);
+    MoveTo(50,50,0.5,0.01);
+    MoveTo(50,80,0.5,0.01);
+    MoveTo(95,80,0.5,0.01);
+    int ch = 0;
+    ch = ScaraTake(ch);
+    if(ch == 2)
+    {
+        MoveTo(0,0,0,0.5,0.01);
+        ch = ScaraTake(ch);
+    }
+    if(ch == 1)
+    {
+        MoveTo(0,0,0,0.5,0.01);
+        ch = ScaraTake(ch);
+    }
+    MoveTo(95,25,0.5,0.01);
+    MoveTo(240,25,0.5,0.01);
+    first_state = Scara_move(0,29,4,first_state);
+    MoveTo(240,65,0.5,0.01);
+    MoveTo(380,50,0.5,0.01);
+    
 }
--- race/src/second.cpp
+++ race/src/second.cpp
@@ -2,27 +2,6 @@
 
 void second()
 {
-<<<<<<< HEAD
-    first_move(60);
-    // MoveTo(200,0,0.5,0.01);
-    odom_init(0,35,0);
-    // first_move(30);
-    MoveTo(0,18,0.5,0.01);
-    MoveTo(80,18,0.5,0.01);
-    // // distance = vl53(2);
-    // // odom_init();
-    Move(0,0.2,0);//move to wall
-    micro_move(100,25,-1,0.2,0.05);
-
-    // MoveTo(80,30,0.5,0.01);
-    // odom_init(-1,25,-1);
-
-    MoveTo(150,25,0.5,0.01);
-    MoveTo(150,55,0.5,0.01);
-    MoveTo(250,55,0.5,0.01);
-    MoveTo(250,20,0.5,0.01);
-    MoveTo(350,20,0.5,0.01);
-=======
     // first_move(60);
     MoveTo(0,25,0.5,0.01);
     MoveTo(80,25,0.5,0.01);
@@ -35,6 +14,5 @@ void second()
     MoveTo(250,70,0.5,0.01);
     MoveTo(250,35,0.5,0.01);
     MoveTo(350,35,0.5,0.01);
->>>>>>> 71b57a08dd857f60af6204b7c1ab873e53c96707
 
 }
