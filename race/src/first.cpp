e2fc6a7cec6d651337ca75815f2b8d154256c113
Leokang0801 <bartkang0818@gmail.com>
2022-11-29 21:44:34 +0000

    version_old

--- race/src/first.cpp
+++ race/src/first.cpp
@@ -2,21 +2,21 @@
 
 void first()
 {
-    first_state = Scara_move(0,0,0,first_state);    
+    // first_state = Scara_move(0,0,0,first_state);    
     // Scara_move(0,29.0832266,0);
-    first_state = Scara_move(-8,22,1,first_state);
-    first_state = Scara_move(0,29,2,first_state);
-    first_state = Scara_move(8,22,3,first_state);
-    first_state = Scara_move(0,29,4,first_state);
-    first_state = Scara_move(0,0,0,first_state);   
+    // first_state = Scara_move(-8,22,1,first_state);
+    // first_state = Scara_move(0,29,2,first_state);
+    // first_state = Scara_move(8,22,3,first_state);
+    // first_state = Scara_move(0,29,4,first_state);
+    // first_state = Scara_move(0,0,0,first_state);   
     
 
     // while(true){
     //     Move(0.5,0.0,0.0);
     // }
-    // MoveTo(50,50,0.5,0.02);
-    // MoveTo(50,80,0.5,0.02);
-    // MoveTo(95,80,0.5,0.02);
+    MoveTo(50,50,0.5,0.02);
+    MoveTo(50,80,0.5,0.02);
+    MoveTo(95,80,0.5,0.02);
     // int ch = 0;
     // ch = ScaraTake(ch);
     // if(ch == 2)
